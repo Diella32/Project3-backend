f1b2ba7ad982ebe30215c09010e13cfedb38cd50
LauraNsenga <laura.nsenga@eagles.oc.edu>
2024-11-13 21:21:09 -0600

    change

--- app/routes/awardCertification.routes.js
+++ app/routes/awardCertification.routes.js
@@ -1,25 +1,25 @@
 module.exports = (app) => {
-  const awardCertification = require("../controllers/awardCertification.controller.js");
+  const AwardCertifications = require("../controllers/awardCertification.controller.js");
   const { authenticate } = require("../authorization/authorization.js");
   const router = require("express").Router();
 
   // Create a new AwardCertification for a resume
-  router.post("/", [authenticate], awardCertification.create);
+  router.post("/", [authenticate], AwardCertifications.create);
 
   // Retrieve all AwardCertifications for a specific resume
-  router.get("/user/:userId", [authenticate], awardCertification.findAllForUser);
+  router.get("/user/:userId", [authenticate], AwardCertifications.findAllForUser);
 
   // Retrieve a single AwardCertification by ID
-  router.get("/:id", [authenticate], awardCertification.findOne);
+  router.get("/:id", [authenticate], AwardCertifications.findOne);
 
   // Update an AwardCertification by ID
-  router.put("/:id", [authenticate], awardCertification.update);
+  router.put("/:id", [authenticate], AwardCertifications.update);
 
   // Delete an AwardCertification by ID
-  router.delete("/:id", [authenticate], awardCertification.delete);
+  router.delete("/:id", [authenticate], AwardCertifications.delete);
 
   // Delete all AwardCertifications for a specific resume
-  router.delete("/resume/:resumeId", [authenticate], awardCertification.deleteAll);
+  router.delete("/resume/:resumeId", [authenticate], AwardCertifications.deleteAll);
 
   
   app.use("/resume-t9/AwardCertifications", router);
