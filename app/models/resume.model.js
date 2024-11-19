dae49be1bd6dc7cbfa51ebf028012e18330d46cf
Diella Mwambutsa <diella.mwambutsa@eagles.oc.edu>
2024-11-19 14:54:11 -0600

    remove in file resume associations

--- app/models/resume.model.js
+++ app/models/resume.model.js
@@ -27,58 +27,5 @@ module.exports = (sequelize, Sequelize) => {
       allowNull: false
     }
   });
-
-  // Define associations
-  Resume.associate = (models) => {
-    // Association with User
-    Resume.belongsTo(models.User, {
-      foreignKey: 'user_id',
-      as: 'user'
-    });
-
-    // Associations with other entities (Skills, Education, etc.)
-    Resume.hasMany(models.Skill, {
-      foreignKey: 'resume_id',
-      as: 'skills',
-      onDelete: 'CASCADE'
-    });
-
-    Resume.hasMany(models.Education, {
-      foreignKey: 'resume_id',
-      as: 'education',
-      onDelete: 'CASCADE'
-    });
-
-    Resume.hasMany(models.PersonalLink, {
-      foreignKey: 'resume_id',
-      as: 'personalLinks',
-      onDelete: 'CASCADE'
-    });
-
-    Resume.hasMany(models.Experience, {
-      foreignKey: 'resume_id',
-      as: 'experiences',
-      onDelete: 'CASCADE'
-    });
-
-    Resume.hasMany(models.Project, {
-      foreignKey: 'resume_id',
-      as: 'projects',
-      onDelete: 'CASCADE'
-    });
-
-    Resume.hasMany(models.Interest, {
-      foreignKey: 'resume_id',
-      as: 'interests',
-      onDelete: 'CASCADE'
-    });
-
-    Resume.hasMany(models.AwardCertification, {
-      foreignKey: 'resume_id',
-      as: 'awardCertifications',
-      onDelete: 'CASCADE'
-    });
-  };
-
   return Resume;
 };
